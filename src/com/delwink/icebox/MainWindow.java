a9a32e7f72d5b7d0f1e257473599e5e097f29190
David McMackins II <contact@mcmackins.org>
2016-09-08 08:23:57 -0500

    Initial commit

---
+++ src/com/delwink/icebox/MainWindow.java
@@ -0,0 +1,25 @@
+/*
+ * IceBox - inventory management software for restaurants
+ * Copyright (C) 2016 Delwink, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.delwink.icebox;
+
+public class MainWindow {
+    
+    public static void main(String[] args) {
+        // TODO: instantiate MainWindow to start application
+    }
+}
